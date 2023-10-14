95c412d6945aabd027ecec8ef511ff564fb96798
Aariz Iqbal <aariz.iqbal@outlook.com>
2023-10-14 15:20:34 -0700

    Start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      The start of my portfolio
+    </div>
+  );
+}
+
+export default App;
